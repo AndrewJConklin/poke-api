db4b7c80e6248fee7ce8c6ebae7c8fa12b7eab4d
aconkli9 <aconkli9@ford.com>
2022-06-03 12:21:56 -0400

    Fix error handling

--- src/main/java/com/coolpokemon/pokeapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/coolpokemon/pokeapi/exception/ResourceNotFoundException.java
@@ -1,10 +1,10 @@
-package com.coolpokemon.exception;
+package com.coolpokemon.pokeapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
   public ResourceNotFoundException(String message) {
     super(message);
   }
