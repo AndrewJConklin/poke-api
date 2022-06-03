780414d3360cb7b5fea2f09849f70336a4899687
aconkli9 <aconkli9@ford.com>
2022-06-03 12:32:29 -0400

    Fix error handling

--- src/main/java/com/coolpokemon/pokeapi/pokemon/exception/ResourceNotFoundException.java
+++ src/main/java/com/coolpokemon/pokeapi/pokemon/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.coolpokemon.pokeapi.exception;
+package com.coolpokemon.pokeapi.pokemon.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
